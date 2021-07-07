9e1e49995b90dfd310da1a4fdb8bbd68d44ccdff
Kamenev Nikolay <79969997909@mail.ru>
2021-07-07 16:31:12 +0300

    create transliteration

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,8 +1,28 @@
-const handleOriginalResponse = (res) => {
+export const handleOriginalResponse = (res) => {
   if (res.ok) {
     return res.json();
   }
   return Promise.reject(res.status);
 }
 
-export default handleOriginalResponse;
+export const transliteration = str => {
+  const keys = {
+    'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd',
+    'е': 'e', 'ё': 'e', 'ж': 'j', 'з': 'z', 'и': 'i',
+    'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o',
+    'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u',
+    'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch', 'ш': 'sh',
+    'ь': '', 'ы': 'y', 'э': 'e', 'ю': 'u', 'я': 'ya',
+    'щ': 'shch', 'ъ': '',
+
+    'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D',
+    'Е': 'E', 'Ё': 'E', 'Ж': 'J', 'З': 'Z', 'И': 'I',
+    'К': 'K', 'Л': 'L', 'М': 'M', 'Н': 'N', 'О': 'O',
+    'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T', 'У': 'U',
+    'Ф': 'F', 'Х': 'H', 'Ц': 'C', 'Ч': 'Ch', 'Ш': 'Sh',
+    'Ь': '', 'Ы': 'Y', 'Э': 'E', 'Ю': 'U', 'Я': 'Ya',
+    'Щ': 'Shch', 'Ъ': ''
+  }
+
+  return str.split("").map(char => typeof keys[char] === "undefined" ? char : keys[char]).join("").replace(/[^a-zа-яё0-9 ]/gi, '').toLowerCase().trim().replace(/\s/g, "-");
+}
