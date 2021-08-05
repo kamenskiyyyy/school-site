b0739cf148d9846c2761c602dbfc847fb6091405
Kamenev Nikolay <79969997909@mail.ru>
2021-08-05 21:22:05 +0300

    delete react

--- src/utils/constants.js
+++
@@ -1,7 +0,0 @@
-export const serverUrl = 'https://api.school-390.kamenev.tech';
-// export const serverUrl = 'http://localhost:3030';
-
-export function formatDate(date) {
-  const d = new Date(date);
-  return ('0' + (d.getDate())).slice(-2) + '.' + ('0' + (d.getMonth() + 1)).slice(-2) + '.' + d.getFullYear();
-}
